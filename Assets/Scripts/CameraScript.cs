ba74c351d39203d2055596d6aa98c2d4d7a5b6fa
NixonWatt <nixon31031@mail.ru>
2016-02-19 02:29:01 +0200

    Добавил стены и препятствия (без действия)

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 targetPos = target.transform.position;
+        transform.position = new Vector3(targetPos.x, 2, -10);
+	}
+}
---
+++ Assets/Scripts/DeathWallScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathWallScript : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 currentPos = transform.position;
+        transform.position = new Vector2(currentPos.x + speed, currentPos.y);
+	}
+}
